646cbd75d73f4f635e546ba187419ebe65bb001a
sheikhreza <71092512+sheikhreza@users.noreply.github.com>
2022-01-10 10:58:38 -0800

    easy easy tamam tamam

--- mainScript.js
+++ mainScript.js
@@ -1,14 +1,3 @@
-//ریفکتور
-//این تابع یک دکوراتور هست که تابع اسم و  تعداد تیم هارا میگیرد
-// و چک های لازم را انجام میدهد
-// const evenChecker = (fn) => {
-//     return (...args) => {
-//         // if (args[])
-//         console.log("args" + args);
-//         alert("args" + args);
-//     }
-// }
-
 //این تابع برای ادد کردن تعدادی اتربیوت هست
 function setAttributes(el, attrs) {
     for (var key in attrs) {
@@ -16,23 +5,114 @@ function setAttributes(el, attrs) {
     }
 }
 
-function addPoints() {
-    rows = document.getElementById("table").rows.length;
-    cells =document.getElementById('table').rows[0].cells.length
-    console.log("rows "+rows + " cells "+cells);
-    resultsTables = document.getElementById("resultsTable")
+// let playedteams =[];
+//این تابع نتایج بازی هارا گرفته و امتیازات را اضافه میکند
+let resualtArray = [];
+handleCreateScoreTable = (TeamScore) => {
+    let mustBePushed;
+    let mustBeAdded;
+    const obj = {
+        teamName: TeamScore[0],
+        score: TeamScore[1],
+        goalFor: TeamScore[2],
+        goalAgainst: TeamScore[3],
+    }
+    if (resualtArray.length === 0) {
+        mustBePushed = true;
+        mustBeAdded = false;
+    }
+    else {
+        for (let i = 0; i < resualtArray.length; i++) {
+            if (resualtArray[i].teamName === TeamScore[0]) {
+                resualtArray[i].score += TeamScore[1];
+                resualtArray[i].goalFor -= -TeamScore[2];
+                resualtArray[i].goalAgainst -= -TeamScore[3];
+                mustBePushed = false;
+                mustBeAdded = true;
+                break;
+            }
+            else {
+                mustBePushed = true;
+                mustBeAdded = false;
+            }
+        }
+    }
+    if (mustBePushed==true&& mustBeAdded == false) {
+        resualtArray.push(obj);
+    }
 
-    // let pointsArray = [];
-    // let teamRecords = [id , points,goalFor , goalAgainst];
-    // console.log(teamRecords);
+    resualtArray?.sort((a, b) => (a.score > b.score ? -1 : 1))
+    arrPrint = document.getElementById('arrPrint');
+    arrPrint.innerHTML = JSON.stringify(resualtArray,null, 4);
+    // resultsTable= document.getElementById("resultsTable")
+    // resultsTable.insertRow()
+}
 
-    for (var i = 0; i < rows; i++) {
-        resultRows = resultsTables.insertRow(i);
-        resultRows.insertCell(0).innerHTML = "نام تیم";
+function handleAddScore(firstCompetitor, secondCompetitor) {
+    let scoresTabels = [];
+    let firstPoint = document.getElementById(firstCompetitor).value;
+    let secondPoint = document.getElementById(secondCompetitor).value;
+    let firstTeamScore = [firstCompetitor, 0, firstPoint, secondPoint];
+    let secondTeamScore = [secondCompetitor, 0, secondPoint, firstPoint];
+    if (firstPoint > secondPoint) {
+        firstTeamScore[1] += 3;
+    } if (firstPoint < secondPoint) {
+        secondTeamScore[1] += 3;
+    } if (firstPoint == secondPoint) {
+        firstTeamScore[1] += 1;
+        secondTeamScore[1] += 1;
+    }
+
+
+    handleCreateScoreTable(firstTeamScore);
+    handleCreateScoreTable(secondTeamScore);
+
+
+    // handleCreateScoreTable(firstTeamScore);
+    // handleCreateScoreTable(secondTeamScore);
+}
+
+function createPointInputes() {
+    table = document.getElementById("table");
+    rows = table.rows.length;
+    cells = table.rows[0].cells.length - 1;
+    inputTables = document.getElementById("pointsTable");
+
+    if (!createPointInputes.didrun) {
+        createPointInputes.didrun = true;
+    }
+    else {
+        for (let i = 0; i < rows; i++) {
+            inputRow = inputTables.insertRow(i);
+            for (let j = 0; j < cells; j++) {
+                inputDiv = document.createElement("div");
+                header = document.createElement("p");
+                header.innerHTML = `نتیجه بازی ${table.rows[i].cells[j].innerHTML}`;
+                firstId = table.rows[i].cells[j].id.split("-")[0];
+                secondId = table.rows[i].cells[j].id.split("-")[1];
+                firsPointInput = document.createElement("input")
+                secondPointInput = document.createElement("input");
+                setAttributes(firsPointInput, {
+                    'type': 'number',
+                    'id': `${firstId}`,
+                    'requires': 'requires',
+
+                });
+                setAttributes(secondPointInput, {
+                    'type': 'number',
+                    'id': `${secondId}`,
+                    'requires': 'requires',
+                    "onchange": `handleAddScore(${firstId},${secondId})`
+                })
+                inputDiv.appendChild(header)
+                inputDiv.appendChild(firsPointInput);
+                inputDiv.appendChild(secondPointInput);
+                inputRow.insertCell(j).appendChild(inputDiv);
+            }
+        }
     }
 
 }
-
 
 
 function matchCreator(teams) {
@@ -53,43 +133,24 @@ function matchCreator(teams) {
     for (let i = 1; i <= weaks; i++) {
         let matches = [];
         matchRows = table.insertRow(i - 1);
-        pointRows = table.insertRow(i);
         cells = matchRows.insertCell(0).innerHTML = ` مسابقات هفته ${i} ام-${i <= weaks / 2 ? 'رفت' : 'برگشت'}`
-        console.log(`مسابقات هفته ی ${i} ام`);
         for (let j = 0; j < teams.length / 2; j++) {
-            firsPointInput = document.createElement("input")
-            secondPointInput = document.createElement("input")
-            setAttributes(firsPointInput, {
-                "placeholder": `گل های زده ${partOne[j]} `,
-                "type": "number",
-                "id": `${partOne[j]}`,
-                "row": `${i}`,
-                "cell": `${j}`,
-                "onchange": "addPoints(this.id , this.value,this.row,this.cell)"
-            });
-            setAttributes(secondPointInput, {
-                "placeholder": `گل های زده ${partTwo[j]} `,
-                "type": "number",
-                "id": `${partTwo[j]}`,
-                "row": `${i + weaks}`,
-                "cell": `${j}`,
-                "onchange": "addPoints(this.id, this.value,this.row,this.cell)"
-            });
             matches.push(`${partOne[j]} - ${partTwo[j]}`);
-            matchRows.insertCell(j).innerHTML = `|${partOne[j]} - ${partTwo[j]}|`;
-            pointRows.insertCell(j).appendChild(firsPointInput);
-            pointRows.insertCell(j).appendChild(secondPointInput);
+            matchRows.insertCell(j).innerHTML = `${partOne[j]} و ${partTwo[j]}`;
+            matchRows.cells[j].id = `${partOne[j]}-${partTwo[j]}`;
         }
-
-
-        console.log(matches);
         partTwo.unshift(partOne[1]);
         partOne.splice(1, 1);
         partOne.push(partTwo.slice(-1));
         partTwo.pop();
     }
-}
 
+    resualtBtn = document.createElement('button');
+    resualtBtn.innerHTML = " دوبار کلیک برای ثبت نتایج";
+    resualtBtn.setAttribute("onclick", "createPointInputes()");
+    resualtBtn.setAttribute("class", "add-btm-2");
+    document.getElementById("matchesContainer").appendChild(resualtBtn);
+}
 
 //تابعی برای اضافه کردن تعدادی اتربیوت به المنت
 
@@ -134,9 +195,8 @@ function checkNameAndCount(leageName, teamCount) {
     if (teamCount % 2 == 0) {
         check = localStorage.getItem(leageName);
         if (check == null) {
-            localStorage.setItem(leageName, leageName);
+            //localStorage.setItem(leageName, leageName);
             creatInputs(teamCount);
-
         }
         else {
             alert("نام " + leageName + " تکراری است،لطفا نام دیگری انتخاب کنید!")
